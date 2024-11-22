4fdd80b32e86b759610bee7605921cb3e3cc705b
CoderVthakre <vaibhavthakre87@gmail.com>
2024-11-22 12:05:11 +0530

    test1

---
+++ src/main/java/com/example/demoPracg/DemoPracgApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoPracg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPracgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPracgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoPracg/DemoPracgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoPracg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPracgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
